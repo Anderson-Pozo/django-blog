9b27f8ada63abb7d99b519f157d073d9984401db
Anderson Pozo <imbander092098@gmail.com>
2020-05-25 12:32:50 -0500

    Update production file

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['https://apdjangoblog.herokuapp.com']
+ALLOWED_HOSTS = ['apdjangoblog.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
